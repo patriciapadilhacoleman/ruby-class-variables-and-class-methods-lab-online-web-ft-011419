f649894088fd788507615f1663ed27f0f4a571ae
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-27 16:32:43 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,55 @@
+class Song
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name,artist, genre)
+    
+    @@count += 1
+    @name = name
+    
+    @artist = artist
+    @@artists << @artist
+    
+    @genre = genre
+    @@genres << @genre
+    
+  end
+  
+  def self.count
+    
+     @@count
+
+  end
+  
+  def self.genres
+    
+    @@genres.uniq
+    
+  end
+  
+  def self.artists
+    
+    @@artists.uniq
+    
+  end
+  
+  def self.genre_count
+    
+    gcount = {}
+    
+    @@genres.uniq.each do |genre|
+      
+      gcount[genre]
+      
+    end
+    
+  end
+  
+  def self.artist_count
+    
+  end
+  
+end
